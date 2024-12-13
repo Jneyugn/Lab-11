d45c9af42810f6447c87ceb38a0845e51c868918
Your Name <you@example.com>
2024-12-13 18:02:27 -0500

    Created base class

---
+++ script.js
@@ -0,0 +1,14 @@
+class ProductProperties {
+    // Constructor method
+    constructor(name, age) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Method to display total value
+    getTotalValue() {
+
+        console.log(`Price: ${this.price}, Quantity: ${this.quantity} Price = ${price*quantity}`);
+    }
+}
